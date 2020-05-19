91472bfaaeef0548e7404462a17e2eca942bd8db
Aymane Boubleh <ayman.boubleh@gmail.com>
2020-05-19 05:17:27 +0000

    first commit

---
+++ src/main/java/ArabicLetterCNN.java
@@ -0,0 +1,137 @@
+import org.datavec.api.io.labels.ParentPathLabelGenerator;
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.split.FileSplit;
+import org.datavec.image.loader.NativeImageLoader;
+import org.datavec.image.recordreader.ImageRecordReader;
+import org.deeplearning4j.api.storage.StatsStorage;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.*;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.ui.api.UIServer;
+import org.deeplearning4j.ui.stats.StatsListener;
+import org.deeplearning4j.ui.storage.InMemoryStatsStorage;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.evaluation.classification.Evaluation;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.learning.config.Sgd;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.io.*;
+import java.util.Random;
+
+public class ArabicLetterCNN {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int nbrEpoch = 5;
+        long seed = 1234;
+        long width = 32;
+        long height = 32;
+        long depth = 1;
+        int batchSize = 54;
+        int outputSize = 28;
+        double learningRate = 0.001;
+        MultiLayerConfiguration multiLayerConfiguration = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .updater(new Adam(learningRate))
+                .list()
+                .setInputType(InputType.convolutionalFlat(height, width, depth))
+                .layer(0, new ConvolutionLayer.Builder()
+                        .nIn(depth)
+                        .nOut(outputSize*2) //number of filters
+                        .activation(Activation.RELU)
+                        .kernelSize(5, 5)
+                        .stride(1, 1)
+                        .build())
+                .layer(1, new SubsamplingLayer.Builder() //zoom en gardant les details qu'on souhaite
+                        .kernelSize(2, 2)
+                        .stride(2, 2)
+                        .poolingType(SubsamplingLayer.PoolingType.MAX)
+                        .build())
+                .layer(2, new ConvolutionLayer.Builder()
+                        .nOut(outputSize*5)
+                        .activation(Activation.RELU)
+                        .kernelSize(5, 5)
+                        .stride(1, 1)
+                        .build())
+                .layer(3, new SubsamplingLayer.Builder()
+                        .poolingType(SubsamplingLayer.PoolingType.MAX)
+                        .kernelSize(2, 2)
+                        .stride(2, 2)
+                        .build())
+                .layer(4, new DenseLayer.Builder()
+                        .nOut(outputSize*50)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(5, new OutputLayer.Builder()
+                        .nOut(outputSize)
+                        .activation(Activation.SOFTMAX)
+                        .lossFunction(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .build())
+                .build();
+        MultiLayerNetwork model = new MultiLayerNetwork(multiLayerConfiguration);
+        model.init();
+        UIServer uiServer = UIServer.getInstance();
+        StatsStorage statsStorage = new InMemoryStatsStorage();
+        uiServer.attach(statsStorage);
+        model.setListeners(new StatsListener(statsStorage));
+//        Il faut avoir le dossier des lettres dans le dossier de l'utilisateur, C:/Users/user
+        String path = System.getProperty("user.home") + "/ArabicLetters";
+        File trainFolder = new File(path + "/training");
+        FileSplit fileSplitTrain = new FileSplit(trainFolder, NativeImageLoader.ALLOWED_FORMATS, new Random(seed));
+        RecordReader recordReaderTrain = new ImageRecordReader(height, width, depth, new ParentPathLabelGenerator());
+        recordReaderTrain.initialize(fileSplitTrain);
+        DataSetIterator dataSetIteratorTrain = new RecordReaderDataSetIterator(recordReaderTrain, batchSize, 1, outputSize);
+
+        DataNormalization scaler = new ImagePreProcessingScaler(0, 1);
+        dataSetIteratorTrain.setPreProcessor(scaler);
+
+
+        System.out.println("----------- Model TRAINING --------------");
+        for (int i = 0; i < nbrEpoch; i++) {
+            System.out.println("Epoch " + i);
+            model.fit(dataSetIteratorTrain);
+        }
+//      Sauvegarde du modele dans un fichier
+    	ModelSerializer.writeModel(model, "arabicletters.model",true);
+
+        System.out.println("-------------- MODEL EVALUATION --------------------");
+        File evalFolder = new File(path + "/testing");
+        FileSplit fileSplitEval = new FileSplit(evalFolder, NativeImageLoader.ALLOWED_FORMATS, new Random(seed));
+        RecordReader recordReaderEval = new ImageRecordReader(height, width, depth, new ParentPathLabelGenerator());
+        recordReaderEval.initialize(fileSplitEval);
+        DataSetIterator dataSetIteratorEval = new RecordReaderDataSetIterator(recordReaderEval, batchSize, 1, outputSize);
+        dataSetIteratorEval.setPreProcessor(scaler);
+        Evaluation evaluation = new Evaluation();
+        while (dataSetIteratorEval.hasNext()) {
+            DataSet dataSet = dataSetIteratorEval.next();
+            INDArray features = dataSet.getFeatures();
+            INDArray targetLabels = dataSet.getLabels();
+            INDArray predictedLabels = model.output(features);
+            evaluation.eval(predictedLabels, targetLabels);
+        }
+        System.out.println(evaluation.stats());
+        System.out.println("------------------------ OK ------------------");
+        System.out.println("------------------------ Confusion Matrix ------------------");
+        System.out.println(evaluation.confusionMatrix());
+        System.out.println("------------------------ Confusion Matrix to CSV ------------------");
+        System.out.println(evaluation.getConfusionMatrix().toCSV());
+        writeUsingOutputStream(evaluation.getConfusionMatrix().toCSV(),"ConfusionMatrix.csv");
+
+    }
+
+    private static void writeUsingOutputStream(String data, String filePath) throws IOException {
+        File file = new File(filePath);
+        FileWriter fr = new FileWriter(file, true);
+        fr.write("\n" + data);
+        fr.close();
+    }
+}
